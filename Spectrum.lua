1d7acab33b4b129f67c6af4abea40b3f79119fc5
RKS <53233461+RKS-GOD@users.noreply.github.com>
2021-09-29 20:46:55 +0100

    Update Spectrum.lua

--- Spectrum.lua
+++ Spectrum.lua
@@ -1,36 +1,7 @@
---[[
-	# DON'T BE A DICK PUBLIC LICENSE
-
-	> Version 1.1, December 2016
-
-	> Copyright (C) [2021] [Sestain]
-
-	Everyone is permitted to copy and distribute verbatim or modified
-	copies of this license document.
-
-	> DON'T BE A DICK PUBLIC LICENSE
-	> TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-
-	1. Do whatever you like with the original work, just don't be a dick.
-
-	   Being a dick includes - but is not limited to - the following instances:
-
-	 1a. Outright copyright infringement - Don't just copy this and change the name.
-	 1b. Selling the unmodified original with no work done what-so-ever, that's REALLY being a dick.
-	 1c. Modifying the original work to contain hidden harmful content. That would make you a PROPER dick.
-
-	2. If you become rich through modifications, related works/services, or supporting the original work,
-	share the love. Only a dick would make loads off this work and not buy the original work's
-	creator(s) a pint.
-
-	3. Code is provided with no warranty. Using somebody else's code and bitching when it goes wrong makes
-	you a DONKEY dick. Fix the problem yourself. A non-dick would submit the fix back.
-]]
-
 local SCRIPT_FILE_NAME = GetScriptName();
-local SCRIPT_FILE_ADDR = "https://raw.githubusercontent.com/Sestain/Aimware-Luas/master/Sestains%20Script/Sestains%20Script.lua";
-local VERSION_FILE_ADDR = "https://raw.githubusercontent.com/Sestain/Aimware-Luas/master/Sestains%20Script/version.txt";
-local VERSION_NUMBER = "1.51";
+local SCRIPT_FILE_ADDR = "https://raw.githubusercontent.com/RKS-GOD/Spectrum.lua/main/Spectrum.lua";
+local VERSION_FILE_ADDR = "https://raw.githubusercontent.com/RKS-GOD/Spectrum.lua/main/version.txt";
+local VERSION_NUMBER = "1.0.0";
 local version_check_done = false;
 local update_downloaded = false;
 local update_available = false;
